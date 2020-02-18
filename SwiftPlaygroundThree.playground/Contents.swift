692b1d343f23f008ac8930eeaa1dbb450fe9f1ff
kat <kjacasio@hawaii.edu>
2020-02-18 10:20:50 -1000

    completed parts 1-7 Feb 18

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,43 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Swift Playgrounds Three 2.18.20
+//Part 2
+
+var fruitNames : [String]
+
+//Part 3
+
+fruitNames = ["Kiwi", "Apples", "Watermelon"]
+
+//Part 4
+
+print(fruitNames)
+
+//Part 5
+
+print("I like to eat \(fruitNames)")
+
+//Part 6 for in loop
+//name is a temporary variable
+
+for name in fruitNames
+{
+    print("I like to eat" + name)
+}
+
+//Part 7 print array indexes
+
+fruitNames = ["Kiwi", "Apples", "Watermelon"]
+
+//Index starts at zero
+
+print(fruitNames[1])
+
+//print item at index 0
+//Problem set #1
+
+
+
+
